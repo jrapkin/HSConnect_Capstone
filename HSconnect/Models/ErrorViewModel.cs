eb32272c774300b3339aeb6ebf8572d2eabcb60e
jrapkin <jrap1243@gmail.com>
2020-04-03 10:22:44 -0500

    initial

---
+++ HSconnect/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HSconnect.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
