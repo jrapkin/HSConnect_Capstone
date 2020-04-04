e28dc8f6288458f48609fefaf88b9bc77f6c132c
jrapkin <jrap1243@gmail.com>
2020-04-04 02:19:42 -0500

    Updated ProvidersController to utilize a repository.

--- HSconnect/Controllers/ProvidersController.cs
+++ HSconnect/Controllers/ProvidersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using HSconnect.Contracts;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,6 +11,11 @@ namespace HSconnect.Controllers
     [Authorize(Roles = "Provider")]
     public class ProvidersController : Controller
     {
+        private IRepositoryWrapper _repo;
+        public ProvidersController (IRepositoryWrapper repo)
+        {
+            _repo = repo;
+        }
         public IActionResult Index()
         {
             return View();
