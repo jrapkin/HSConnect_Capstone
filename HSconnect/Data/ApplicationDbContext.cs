81bc3e9f5177595b279d93b4f422e0131ed0e2df
jrapkin <jrap1243@gmail.com>
2020-04-04 00:51:09 -0500

    Created SocialWorker model and Provider model. Implemented global routing filter to route social workers and providers to their respective views. If a user is registered they will be routed to the appropriate index view.

--- HSconnect/Data/ApplicationDbContext.cs
+++ HSconnect/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,6 +12,24 @@ namespace HSconnect.Data
 		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
 			: base(options)
 		{
+		}
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+			builder.Entity<IdentityRole>()
+				.HasData(
+					new IdentityRole
+					{
+						Name = "Social Worker",
+						NormalizedName = "SOCIALWORKER"
+					},
+
+					new IdentityRole
+					{
+						Name = "Provider",
+						NormalizedName = "PROVIDER"
+					}
+				);
 		}
 	}
 }
--- HSconnect/Startup.cs
+++ HSconnect/Startup.cs
@@ -12,6 +12,9 @@ using HSconnect.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using HSconnect.ActionFilters;
 
 namespace HSconnect
 {
@@ -34,6 +37,11 @@ namespace HSconnect
 				.AddEntityFrameworkStores<ApplicationDbContext>()
 				.AddDefaultUI()
 				.AddDefaultTokenProviders();
+			services.AddScoped<ClaimsPrincipal>(s => s.GetService<IHttpContextAccessor>().HttpContext.User);
+			services.AddControllers(config =>
+			{
+				config.Filters.Add(typeof(GlobalRouting));
+			});
 			services.AddControllersWithViews();
 			services.AddRazorPages();
 		}
