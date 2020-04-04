d00af227d74d4bbf76a217bb404f147982485832
jrapkin <jrap1243@gmail.com>
2020-04-04 01:48:52 -0500

    Created the ISocialWorkerRepository interface and IProviderRepository interface, created the ProviderRepository and SocialWorkerRepository which are both contracted with their respective interfaces and inherit from the RepositoryBase abstract class.

---
+++ HSconnect/Contracts/IProviderRepository.cs
@@ -0,0 +1,12 @@
+﻿using HSconnect.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HSconnect.Contracts
+{
+	public interface IProviderRepository : IRepositoryBase<Provider>
+	{
+	}
+}
