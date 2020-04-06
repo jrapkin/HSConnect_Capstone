850d9827b92034cfb725e4452b490e633ee2e929
jrapkin <jrap1243@gmail.com>
2020-04-06 11:01:08 -0500

    Added Corresponding repositories and interfaces for new models.

---
+++ HSconnect/Data/CategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using HSconnect.Contracts;
+using HSconnect.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HSconnect.Data
+{
+	public class CategoryRepository : RepositoryBase<Category>, ICategoryRepository
+	{
+		public CategoryRepository(ApplicationDbContext applicationDbContext)
+			: base(applicationDbContext)
+		{
+		}
+	}
+}
---
+++ HSconnect/Data/DemographicRepository.cs
@@ -0,0 +1,17 @@
+﻿using HSconnect.Contracts;
+using HSconnect.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HSconnect.Data
+{
+	public class DemographicRepository : RepositoryBase<Demographic>, IDemographicRepository
+	{
+		public DemographicRepository(ApplicationDbContext applicationDbContext)
+			: base(applicationDbContext)
+		{
+		}
+	}
+}
