2631313a93d8ba88d60fa57cb5cd52416ccebe52
Matthew VH <vanheckematt@gmail.com>
2020-04-06 10:53:57 -0500

    Built method in MailKitAPI to SendEmail. This method will take in a MimeMessage object as well as strings for username and password as its parameters.

--- HSconnect/Services/MailKitAPI.cs
+++ HSconnect/Services/MailKitAPI.cs
@@ -1,14 +1,29 @@
-﻿using MimeKit;
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+//using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace HSconnect.Services
 {
     public static class MailKitAPI
     {
-        public static MimeMessage CreateEmail (MailboxAddress fromAddress, List<MailboxAddress> toAddresses, string textContent)
+        public static MimeMessage CreateEmail(string from, List<string> to, string messageBodyText)
+        {
+            string[] fromAddressParts = from.Split(", ");
+            MailboxAddress fromAddress = new MailboxAddress(fromAddressParts[0], fromAddressParts[1]);
+            List<MailboxAddress> toAddresses = new List<MailboxAddress>();
+            foreach (string address in to)
+            {
+                string[] toAddressParts = address.Split(", ");
+                toAddresses.Add(new MailboxAddress(toAddressParts[0], toAddressParts[1]));
+            }
+            return CreateEmail(fromAddress, toAddresses, messageBodyText);
+        }
+        public static MimeMessage CreateEmail(MailboxAddress fromAddress, List<MailboxAddress> toAddresses, string messageBodyText)
         {
             MimeMessage message = new MimeMessage();
             message.From.Add(fromAddress);
@@ -18,13 +33,20 @@ namespace HSconnect.Services
             }
             message.Body = new TextPart("plain")
             {
-                Text = textContent
+                Text = messageBodyText
             };
             return message;
         }
-        public static void SendEmail (MimeMessage message)
+        public static void SendEmail (MimeMessage message, string username, string password)
         {
-
+            using (SmtpClient client = new SmtpClient())
+            {
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                client.Connect("smtp.gmail.com", 587, false);
+                client.Authenticate(username, password);
+                client.Send(message);
+                client.Disconnect(true);
+            };
         }
     }
 }
