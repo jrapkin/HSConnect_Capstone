ae170a19e9012280195793e0577aa9244a80e068
Matthew VH <vanheckematt@gmail.com>
2020-04-06 08:32:34 -0500

    Built CreateEmail method in MailKitAPI which returns a MimeMessage object. Next goal: Build SendEmail method which will take in a MimeMessageObject.

---
+++ HSconnect/Services/MailKitAPI.cs
@@ -0,0 +1,30 @@
+﻿using MimeKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HSconnect.Services
+{
+    public static class MailKitAPI
+    {
+        public static MimeMessage CreateEmail (MailboxAddress fromAddress, List<MailboxAddress> toAddresses, string textContent)
+        {
+            MimeMessage message = new MimeMessage();
+            message.From.Add(fromAddress);
+            foreach (MailboxAddress address in toAddresses)
+            {
+                message.To.Add(address);
+            }
+            message.Body = new TextPart("plain")
+            {
+                Text = textContent
+            };
+            return message;
+        }
+        public static void SendEmail (MimeMessage message)
+        {
+
+        }
+    }
+}
