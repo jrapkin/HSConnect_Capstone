5edf7cd0ec496f608cb63343c0419db1078e246c
Matthew VH <vanheckematt@gmail.com>
2020-04-06 18:34:57 -0500

    Began writing code in chat.js.

---
+++ HSconnect/wwwroot/js/chat.js
@@ -0,0 +1,7 @@
+//"use strict";
+
+//var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//document.getElementById("sendButton").disabled = true;
+
+//connection.on("ReceiveMessage",)
