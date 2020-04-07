72869ce80e2831036c4df21e19173fc78565efb4
Matthew VH <vanheckematt@gmail.com>
2020-04-07 17:56:10 -0500

    In the process of making the chat window toggleable.

--- HSconnect/wwwroot/js/site.js
+++ HSconnect/wwwroot/js/site.js
@@ -1,10 +1,23 @@
 ﻿"use strict";
-var isOpen = false;
+var chatIsOpen = false;
 function openChat(nameOfUser) {
-    document.getElementById("chatApp").innerHTML = "<div class=\"container\"><div class=\"row\">&nbsp;</div><div class=\"row\"><div class=\"col-2\">User</div><div class=\"col-4\"><input type=\"text\" id=\"userInput\" value=\"" + nameOfUser + "\" /></div></div><div class=\"row\"><div class=\"col-2\">Message</div><div class=\"col-4\"><input type=\"text\" id=\"messageInput\" /></div></div><div class=\"row\">&nbsp;</div><div class=\"row\"><div class=\"col-6\"><input type=\"button\" id=\"sendButton\" value=\"Send Message\" /></div></div></div><div class=\"row\"><div class=\"col-12\"><hr /></div></div><div class=\"row\"><div class=\"col-6\"><ul id=\"messagesList\"></ul></div></div>";
     var scriptTag = document.createElement("SCRIPT");
     var srcForScriptTag = document.createAttribute("SRC");
     srcForScriptTag.value = "/js/chat.js";
     scriptTag.setAttributeNode(srcForScriptTag);
-    document.querySelector("body").appendChild(scriptTag);
+
+    if (!chatIsOpen) {
+        document.getElementById("chatApp").innerHTML = "<div class=\"container\"><div class=\"row\">&nbsp;</div><div class=\"row\"><div class=\"col-2\">User</div><div class=\"col-4\"><input type=\"text\" id=\"userInput\" value=\"" + nameOfUser + "\" /></div></div><div class=\"row\"><div class=\"col-2\">Message</div><div class=\"col-4\"><input type=\"text\" id=\"messageInput\" /></div></div><div class=\"row\">&nbsp;</div><div class=\"row\"><div class=\"col-6\"><input type=\"button\" id=\"sendButton\" value=\"Send Message\" /></div></div></div><div class=\"row\"><div class=\"col-12\"><hr /></div></div><div class=\"row\"><div class=\"col-6\"><ul id=\"messagesList\"></ul></div></div>";
+        document.getElementById("toggleChat").innerText = "Close Chat";
+        document.querySelector("body script:last-child").setAttribute("SRC", "/js/chat.js");
+        document.querySelector("body").appendChild(scriptTag);
+        chatIsOpen = true;
+    }
+    else {
+        document.getElementById("chatApp").innerHTML = "";
+        document.getElementById("toggleChat").innerText = "Open Instant Chat";
+        //document.getElementById("chatJs").setAttribute("SRC", "");
+        chatIsOpen = false;
+        //document.querySelector("body").removeChild(scriptTag);
+    };
 }
