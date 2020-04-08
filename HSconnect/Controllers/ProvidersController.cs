e84b931ba2a579d34667a5349846244a26b13e3b
unknown <christykyang@gmail.com>
2020-04-08 09:32:49 -0500

    added methods in repos. Continue working on create methods for serviceOffered and partnerships

--- HSconnect/Controllers/ProvidersController.cs
+++ HSconnect/Controllers/ProvidersController.cs
@@ -8,7 +8,6 @@ using HSconnect.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
