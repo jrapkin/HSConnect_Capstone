b2d31f01da55054de9efc0ec359f98ddf94c5ab1
Matthew VH <vanheckematt@gmail.com>
2020-04-08 16:31:04 -0500

    Resized the chat window to be larger. Also made its size responsive depending on the screen width.

--- HSconnect/wwwroot/js/site.js
+++ HSconnect/wwwroot/js/site.js
@@ -7,7 +7,38 @@ function openChat(nameOfUserFrom, nameOfUserTo, savedMessages) {
     scriptTag.setAttributeNode(srcForScriptTag);
 
     if (!chatIsOpen) {
-        document.getElementById("chatApp").innerHTML = "<div class=\"container\"><div class=\"row\">&nbsp;</div><div class=\"row\"><div class=\"col-2\">User</div><div class=\"col-4\"><input type=\"text\" id=\"userFromInput\" value=\"" + nameOfUserFrom + "\" /><input type=\"text\" id=\"userToInput\" value=\"" + nameOfUserTo + "\" /></div></div><div class=\"row\"><div class=\"col-2\">Message</div><div class=\"col-4\"><input type=\"text\" id=\"messageInput\" /></div></div><div class=\"row\">&nbsp;</div><div class=\"row\"><div class=\"col-6\"><input type=\"button\" id=\"sendButton\" value=\"Send Message\" /></div></div></div><div class=\"row\"><div class=\"col-12\"><hr /></div></div><div class=\"row\"><div class=\"col-6\"><ul id=\"messagesList\"></ul></div></div>";
+        document.getElementById("chatApp").innerHTML =
+            "<div class=\"container\">" +
+                "<div class=\"row\">&nbsp;</div>" +
+                "<div class=\"row\">" +
+                    "<div class=\"col-2\">User</div >" +
+                "<div class=\"col-4\">" +
+                    "<input type=\"text\" id=\"userFromInput\" value=\"" + nameOfUserFrom + "\" />" +
+                    "<input type=\"text\" id=\"userToInput\" value=\"" + nameOfUserTo + "\" />" + 
+                "</div >" +
+            "</div >" +
+            "<div class=\"row\">" +
+                "<div class=\"col-2\">Message</div>" +
+                "<div class=\"col-4\">" +
+                    "<input type=\"text\" id=\"messageInput\" />" +
+                "</div>" + 
+            "</div >" +
+            "<div class=\"row\">&nbsp;</div>" +
+                "<div class=\"row\">" +
+                    "<div class=\"col-6\">" +
+                    "<input type=\"button\" id=\"sendButton\" value=\"Send Message\" />" + 
+                    "</div >" + 
+                "</div >" + 
+            "</div >" +
+            "<div class=\"row\">" +
+                "<div class=\"col-12\">" +
+                "<hr />" + 
+                "</div >" +
+            "</div >" +
+            "<div class=\"row\"><div>" +
+            "<ul id=\"messagesList\"></ul>" + 
+            "</div >" +
+            "</div > ";
         //for (let i = 0; i < savedMessages.length; i++) {
         //    let savedMessage = document.createElement("li");
         //    savedMessage.textContent = messages[i];
