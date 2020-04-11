736b551bf2b48d01106ef0ffe4882bc63b1b2b4a
christykyang@gmail.com <christykyang@gmail.com>
2020-04-11 11:44:31 -0500

    identified what API I wanted to use. Got Key. Tested in Postman. Generated Model. Created interface and Service Class.

---
+++ HSconnect/Contracts/IGetCoordinatesRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HSconnect.Models;
+
+namespace HSconnect.Contracts
+{
+    public interface IGetCoordinatesRequest
+    {
+        Task<GetCoordinates> GetCoordinates();
+    }
+}
---
+++ HSconnect/Services/GeocodeAPI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using HSconnect.Contracts;
+using HSconnect.Models;
+
+namespace HSconnect.Services
+{
+    //static?
+    public class GeocodeAPI : IGetCoordinatesRequest
+    {
+        public GeocodeAPI()
+        {
+
+        }
+        public async Task<GetCoordinates> GetCoordinates()
+        {
+            string url = $"https://maps.googleapis.com/maps/api/geocode/json?address=313+plankinton+ave,milwaukee,+wi&key={API_Keys.GeocodeAndGoogleMapsKey}";
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(url);
+            if(response.IsSuccessStatusCode)
+            {
+                string json = await response.Content.ReadAsStringAsync();
+                GetCoordinates coordinates = JsonConvert.DeserializeObject<GetCoordinates>(json);
+                return coordinates;
+            }
+            return null;
+
+        }
+
+        //public async Task<double[]> GetCoordinatesUsingGeocode(string url)
+        //{
+        //    HttpClient client = new HttpClient();
+        //    using(client)
+        //    {
+        //        HttpResponseMessage response = await client.GetAsync(url);
+        //        string data = await response.Content.ReadAsStringAsync();
+        //        JObject dataAsJObject = JsonConvert.DeserializeObject<JObject>(data);
+        //        double lat = 
+        //    }
+        //}
+    }
+}
